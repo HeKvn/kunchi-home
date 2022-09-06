009ebaffaf3dd96f073a9649bdb38aeb0b5234f8
HeKvn <377014982@qq.com>
2022-09-06 22:10:24 +0800

    样式优化，增加导航按钮

--- src/views/body/index.tsx
+++ src/views/body/index.tsx
@@ -1,10 +1,12 @@
 import Search from './search/search'
+import Navigation from './navigation/navigation'
 import style from './body.module.scss'
 
 export default function Body() {
   return (
     <div className={style.Body}>
       <Search />
+      <Navigation />
     </div>
   )
 }
